1211477b56347a265a42e50250650822d49905a5
SatoshiFurutani <chips.potato.tomato@gmail.com>
2023-07-08 18:43:34 +0900

    Update Enemymove.cs

--- Assets/Scripts/Enemymove.cs
+++ Assets/Scripts/Enemymove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMove : MonoBehaviour
+public class Enemymove : MonoBehaviour
 {
     [SerializeField] private float enemySpeed;
     [SerializeField] private float enemyLifeTime;
